54d7e77f8a3e20cb50db64176c34993511ddb29d
Herrera Garay <KHerrera@bnb.com.bo>
2020-01-23 11:20:05 -0400

    primer commit

---
+++ PruebaNUnit/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("PruebaNUnit")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PruebaNUnit")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("869a64e0-39e1-4722-9ee4-052b15be2cb9")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ PruebaNUnit/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace PruebaNUnit
+{
+   
+    public class UnitTest1
+    {
+        [Test]
+        public void TestMethod1()
+        {
+            Console.WriteLine("------------------------TestMethod1--------------------");            
+            Assert.IsTrue(true);
+        }
+    }
+}
